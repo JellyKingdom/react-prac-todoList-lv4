c038da886c150c452640522b15114c8c81b4ca89
JellyKingdom <jyhan1993@gmail.com>
2023-02-21 09:43:22 +0900

    axios: get notes list

--- src/features/notes/components/List.jsx
+++ src/features/notes/components/List.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 function List() {
+    const [notes, setNotes] = useState(null);
+
     const navigate = useNavigate();
+    
+    const fetchNotes = async () => {
+        const {data} = await axios.get('http://localhost:4000/notes/');
+        console.log(data);
+        setNotes(data);
+
+    }
+    useEffect(()=>{
+        //db로부터 값 가져오기
+        fetchNotes();
+    },[]);
     return(
         <>
             <button onClick={()=>{
                 navigate("notes/add");
             }}>추가하기</button>
+            <div>
+            {
+                notes?.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            {item.id} : {item.title}
+                        </div>
+                    )
+                })
+            }
+            </div>
+
         </>
     );
 }
